33f093b06e2a7a04012b4e0590d7516f08f41f74
bengimbel <bgimbel@solstice.com>
2017-12-15 12:49:20 -0600

    made feed layout

---
+++ Social-Network-Project/View/PostCell.swift
@@ -0,0 +1,27 @@
+//
+//  PostCell.swift
+//  Social-Network-Project
+//
+//  Created by Ben Gimbel on 12/15/17.
+//  Copyright © 2017 Ben Gimbel. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+
+    
+
+}
